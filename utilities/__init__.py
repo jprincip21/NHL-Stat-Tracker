db1069b1b00d87333ace85bb2c38c90783a366e1
jprincip21 <jprincip21@gmail.com>
2025-05-23 20:37:36 -0400

    Added svg conversion for team logos

--- utilities/__init__.py
+++ utilities/__init__.py
@@ -1,4 +1,4 @@
 from .api_handler import get_games_by_date
-from .get_images import get_image_default, get_image_light_dark
+from .get_images import get_image_default, get_image_light_dark, get_team_logo
 
-__all__ = ["get_games_by_date", "get_image_default", "get_image_light_dark"]
+__all__ = ["get_games_by_date", "get_image_default", "get_image_light_dark", "get_team_logo"]
--- utilities/get_images.py
+++ utilities/get_images.py
@@ -2,6 +2,10 @@ import tkinter as tk
 from tkinter import ttk
 import customtkinter as ctk
 
+import requests
+import cairosvg
+from io import BytesIO
+
 from PIL import Image
 
 def get_image_light_dark(base_path):
@@ -15,3 +19,16 @@ def get_image_default(base_path):
     return ctk.CTkImage(light_image=Image.open(base_path),
                         dark_image=Image.open(base_path),
                         size=(50, 50))
+
+def get_team_logo(svg_url, size=(50,50)):
+    response = requests.get(svg_url)
+
+    if response.status_code != 200:
+        print("Failed to load logo.")
+        return None
+    
+    png_bytes = cairosvg.svg2png(bytestring=response.content, output_width=size[0], output_height=size[1])
+
+    return ctk.CTkImage(light_image=Image.open(BytesIO(png_bytes)),
+                        dark_image=Image.open(BytesIO(png_bytes)),
+                        size=(28, 28))
