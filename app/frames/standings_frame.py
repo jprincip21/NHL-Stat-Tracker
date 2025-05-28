197450973a482fd2890285b5f3787034cd841e39
jprincip21 <jprincip21@gmail.com>
2025-05-27 22:52:34 -0400

    added logic to display standings by Overall, Conference and Division

--- app/frames/standings_frame.py
+++ app/frames/standings_frame.py
@@ -4,7 +4,7 @@ import customtkinter as ctk
 
 import threading
 
-from utilities import get_standings
+from utilities import get_standings, get_team_logo
 from assets.constants import *
 
 class StandingsFrame(ctk.CTkFrame):
@@ -48,7 +48,39 @@ class StandingsFrame(ctk.CTkFrame):
         self.conference_btn.configure(command=lambda: self.change_filter(self.conference_btn, "Conference"))
         self.overall_btn.configure(command=lambda: self.change_filter(self.overall_btn, "Overall"))
         
-        self.progress_bar = ctk.CTkProgressBar(self, width=600)
+        self.standings_main_frame = ctk.CTkScrollableFrame(self, height=430)
+
+        self.standings_main_frame.columnconfigure(0, weight=1)
+        self.standings_main_frame.columnconfigure(1, weight=0)
+        self.standings_main_frame.columnconfigure(2, weight=0)
+        self.standings_main_frame.columnconfigure(3, weight=0)
+        self.standings_main_frame.columnconfigure(4, weight=0)
+        
+        self.standings_main_frame.grid(row=3, column=0, padx=PADX, pady=PADY, sticky="ew")
+
+        labels_frame = ctk.CTkFrame(self.standings_main_frame) 
+        labels_frame.grid(row=0, column=0, padx=PADX, pady=PADY) 
+
+        headers = [
+                ("Team", 250),
+                ("GP", 30),
+                ("Pts", 30),
+                ("W-L-OTL", 50),
+                ("Pts%", 30),
+                ("RW", 30),
+                ("ROW", 30),
+                ("GF", 30),
+                ("GA", 30),
+                ("GD", 30),
+                ("SO W-L", 40),
+                ("Last 10", 40),
+                
+            ]
+        
+        for i, (text, width) in enumerate(headers):
+            self.create_label(labels_frame, text, width, i)
+
+        self.progress_bar = ctk.CTkProgressBar(self.standings_main_frame, width=600, determinate_speed=0.25)
         self.overall_standings = None    
         self.conference_standings = {"Eastern" : [], 
                                      "Western" : []}
@@ -116,7 +148,7 @@ class StandingsFrame(ctk.CTkFrame):
 
         self.progress_bar.grid_remove() 
         self.progress_bar.stop()
-        self.change_filter(self.overall_btn, "Overall") #Sets Default Frame
+        self.change_filter(self.division_btn, "Division") #Sets Default Frame
         # for team in standings:
         #     print(team)
         
@@ -133,7 +165,6 @@ class StandingsFrame(ctk.CTkFrame):
             self.conference_standings[team["conference"]].append(team) #Seperate Teams By conference
             self.division_standings[team["division"]].append(team) # Seperate Teams by Division
 
-
         self.overall_standings.sort(key=lambda x: x["points"], reverse=True) #Sort Standings
         
         for conf in self.conference_standings:
@@ -143,41 +174,78 @@ class StandingsFrame(ctk.CTkFrame):
             self.division_standings[div].sort(key=lambda x:x["points"], reverse=True) #Sort Division Standings
 
     def render_standings(self, filter_type):
-        
+        """Function which renders standings by sent filter type"""
         if hasattr(self, "standings_display_frame"):
-            standings_display_frame.destroy()
+            self.standings_display_frame.destroy()
 
-        standings_display_frame = ctk.CTkScrollableFrame(self, height=411)
-        standings_display_frame.columnconfigure(0, weight=1)
-        
-        standings_display_frame.grid(row=3, column=0, padx=PADX, pady=PADY, sticky="ew")
+        self.standings_display_frame = ctk.CTkFrame(self.standings_main_frame)
+        self.standings_display_frame.grid(row=1, column=0, padx=PADX, pady=PADY)
 
         if filter_type == "Division":
-            division_label = ctk.CTkLabel(standings_display_frame, text="Division", font=("IMPACT", 24)) #Create Label
-            division_label.grid(row=0, column=0, padx=PADX, pady=PADY, sticky="ew") #Place Label 
 
-            for div, teams in self.division_standings.items(): #Testing
-                print(f"\n{div}")
-                for team in teams:
-                    print(team["team_name"])
-    
-
+            self.render_group_standings(self.division_standings, self.standings_display_frame)
+            
         elif filter_type == "Wild Card":
-            wildcard_label = ctk.CTkLabel(standings_display_frame, text="Wild Card", font=("IMPACT", 24)) #Create Label
-            wildcard_label.grid(row=0, column=0, padx=PADX, pady=PADY, sticky="ew") #Place Label     
+            pass
 
         elif filter_type == "Conference":
-            conference_label = ctk.CTkLabel(standings_display_frame, text="Conference", font=("IMPACT", 24)) #Create Label
-            conference_label.grid(row=0, column=0, padx=PADX, pady=PADY, sticky="ew") #Place Label      
-            
-            for conf_name, teams in self.conference_standings.items(): #Testing
-                print(f"\n{conf_name}")
-                for team in teams:
-                    print(team["team_name"])
+            self.render_group_standings(self.conference_standings, self.standings_display_frame)        
 
         elif filter_type == "Overall":
-            overall_label = ctk.CTkLabel(standings_display_frame, text="Overall", font=("IMPACT", 24)) #Create Label
-            overall_label.grid(row=0, column=0, padx=PADX, pady=PADY, sticky="ew") #Place Label    
-            print("\n")
-            for team in self.overall_standings: #Testing
-                print(team["team_name"])  
+            for row, team in enumerate(self.overall_standings): 
+                self.render_team_row(row, team, self.standings_display_frame)
+
+    def render_group_standings(self, grouped_data, frame):
+        """Function which creates a label of category and then calls render team row to display teams in that category"""
+        current_row = 0
+        for group_name, teams in grouped_data.items(): #Testing
+            conference_label = ctk.CTkLabel(self.standings_display_frame,
+                                            text=group_name,
+                                            font=("IMPACT", 20),
+                                            anchor="w")
+            conference_label.grid(row=current_row, 
+                                    column=0, 
+                                    columnspan=12, 
+                                    sticky="w", 
+                                    padx=10,
+                                    pady=10)
+            current_row +=1
+
+            for team in teams:
+                self.render_team_row(current_row, team, frame)
+                current_row += 1                
+
+
+    def render_team_row(self, row, team, frame):
+        """Function which makes a tuple of team info and label size"""
+        logo = team["team_logo"]
+        team_stats = [
+            (f"{team['team_name']}", 250),
+            (f"{team['games_played']}", 30),
+            (f"{team['points']}", 30),
+            (f"{team['record']}", 50),
+            (f"{round(team['points_percent'], 3)}", 30),
+            (f"{team['regulation_wins']}", 30),
+            (f"{team['regulation_ot_wins']}", 30),
+            (f"{team['goals_for']}", 30),
+            (f"{team['goals_against']}", 30),
+            (f"{team['goal_diff']}", 30),
+            (f"{team['shootout']}", 40),
+            (f"{team['last_ten']}", 40),
+        ]
+        for col, (text, width) in enumerate(team_stats):
+            self.create_label(frame, text, width, col, row=row, image=logo)
+
+
+    def create_label(self, parent, text, width, col, row=0, image=None):
+        """Function which creates labels"""
+        label = ctk.CTkLabel(
+                    parent,
+                    image=image if col==0 and image else None, #Image is default None, If an Image is sent, only display on Column 0
+                    compound="left",
+                    text=text,
+                    font=("IMPACT", 14),
+                    width=width,
+                    anchor="w"
+                )
+        label.grid(row=row, column=col, sticky="ew", padx=5, pady=5)
