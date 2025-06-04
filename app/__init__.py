f86cc0b865e666c09c66f020e674b1f482b98b09
jprincip21 <jprincip21@gmail.com>
2025-06-04 19:18:55 -0400

    Added database imports

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
-__version__ = "0.0.1"
+__version__ = "1.0.0"
 
-from utilities import get_games_by_date, get_image_default, get_image_light_dark
-from .gui import Application
+from utilities import get_games_by_date, get_image_default, get_image_light_dark # UTILITY IMPORTS
+from .database.db_handler import initialize_database, update_display_mode, get_display_mode #DATABASE IMPORTS
+from .gui import Application 
 
 __all__ = [
     "Application",
     "get_games_by_date",
     "get_image_default",
-    "get_image_light_dark"
+    "get_image_light_dark",
+    "initialize_database",
+    "update_display_mode",
+    "get_display_mode"
 ]
